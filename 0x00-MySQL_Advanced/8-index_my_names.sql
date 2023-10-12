02faa3ed3d1f018bc21cc71225a64b93fcf2fba0
Agunloye Olaniyi <nicolastfile@gmail.com>
2023-10-12 16:41:55 +0100

    Worked on more SQL Queries

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,4 +1,5 @@
 -- Creates an index idx_name_first on the first letter of name in the names table.
 
 -- Create the index idx_name_first
-CREATE INDEX idx_name_first ON names (LEFT(name, 1));
+CREATE INDEX idx_name_first
+ ON names(name(1));
